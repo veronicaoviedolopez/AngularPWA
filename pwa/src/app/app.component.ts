069461b35096faf54330cef178e6aae946f4aeba
veronica <veronicaoviedolopez@gmail.com>
2019-12-20 15:19:27 -0600

    before update to angular 8

---
+++ pwa/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pwa';
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+}
