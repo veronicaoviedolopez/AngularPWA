adc9e59dca27c6360bdc580f1096016eb33ac832
veronica <veronicaoviedolopez@gmail.com>
2019-12-29 19:56:14 -0600

    login view

--- pwa/src/app/authentication/authentication.module.ts
+++ pwa/src/app/authentication/authentication.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { MaterialModule } from '@material/material.module';
 
 
 
@@ -11,7 +12,8 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthenticationModule { }
